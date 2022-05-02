e04d438b95d8801d2194bef279c54ad6ca63ac4e
Mateusz Zebik <zebik.mateusz@gmail.com>
2022-05-02 23:29:39 +0200

    Dodawanie i odejmowanie

---
+++ src/main/java/com/kodilla/kalkulator/Calculator.java
@@ -0,0 +1,15 @@
+package com.kodilla.kalkulator;
+
+public class Calculator {
+
+    int A;
+    int B;
+
+    public int addition(){
+        return A + B;
+    }
+
+    public int subtraction(){
+        return A - B;
+    }
+}
---
+++ src/main/java/com/kodilla/kalkulator/KalkulatorApplication.java
@@ -0,0 +1,16 @@
+package com.kodilla.kalkulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.*;
+import java.io.*;
+
+@SpringBootApplication
+
+public class KalkulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KalkulatorApplication.class, args);
+	}
+
+}
